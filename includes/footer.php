49988a1657d4760aeeb32ab4c22b6777fb213092
Subhodip <subhodip.banerjee29@gmail.com>
2017-02-14 23:58:47 +0530

    activation page added

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+    <a href="#" class="scrollup"><i class="fa fa-chevron-circle-up icon_for_scroll_up"></i></a>
+    <div id="fb-root"></div>
+
+    <div class="footerbar" style="">
+      <div class="container clearfix">
+        <div class="row">
+          <div class="call-us">
+            <span class="dis-blk" style=" font-size: 14px">
+              Call us on &nbsp; <i class="fa fa-phone"></i>            
+            </span>
+            <span id="contact-ind" style=" font-size: 14px" data-val="+91-88808-62004">
+              &nbsp;&nbsp;+91 7278003722&nbsp;&nbsp;
+            </span>
+          </div>  
+          <div class="fb-like" data-href="https://www.facebook.com/KnowledgeLab-World-865527740189412/" data-layout="standard" data-action="like" data-show-faces="false" data-share="true" style="padding-top: 10px;width: 100px">
+          </div>
+          <p class="copyright pull-right" style=" color: white;padding-top: 10px;margin-right: 300px; font-size: 12px"> Copyright <i class="fa fa-copyright" aria-hidden="true"></i> Knowledgelab 2016. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
